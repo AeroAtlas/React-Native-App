a34472b828682320c9e0adffe23f719f57faca2b
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-10 23:50:45 -0700

    added basic layout for the react native app

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+//import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  //start state -> after state
+  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={ {padding: 30} }>
+      <View id="viewInput">
+        <Text/>
+        <Button title="ADD"/>
+      </View>
+      <View id="viewOutput">
+        ...
+      </View>
+      {/* <Text>{outputText}</Text>
+      <Button title="Click Here" onPress={() => setOutputText('The text changed!')}/> */}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
