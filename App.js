86c4f4fbdf81ed06f753f48d7d254677bdf7f46d
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-11 22:04:38 -0700

    added text input and set up views

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 //import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
-  //start state -> after state
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={ {padding: 30} }>
-      <View id="viewInput">
-        <Text/>
+    <View id="viewContainer" style={ {padding: 50} }>
+      <View id="viewInput" style={{
+        flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+      }}>
+        <TextInput
+          placeholder="Goal"
+          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
+        />
         <Button title="ADD"/>
       </View>
       <View id="viewOutput">
-        ...
       </View>
-      {/* <Text>{outputText}</Text>
-      <Button title="Click Here" onPress={() => setOutputText('The text changed!')}/> */}
     </View>
   );
 }
