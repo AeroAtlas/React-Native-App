308b4ef81270121221d6b5ad27f2e4c5f5a1798c
Andrew Hardemon <aeroatlas@ymail.com>
2020-08-11 23:33:02 -0700

    fixed bug and got scrolling to work

--- App.js
+++ App.js
@@ -1,25 +1,40 @@
 //import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
+  const [enteredText, setEnteredText] = useState('');
+  const [storedText, setStoredText] = useState([]);
+  const textInputHandler = newText => setEnteredText(newText);
+  const addTextHandler = () => setStoredText(currentText => [...currentText, enteredText]);
   return (
-    <View id="viewContainer" style={ {padding: 50} }>
-      <View id="viewInput" style={{
-        flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
-      }}>
+    <View id="viewContainer" style={styles.viewRootContainer}>
+      <View id="viewInputContainer" style={styles.viewInputContainer}>
         <TextInput
-          placeholder="Goal"
-          style={{ width: '80%', borderColor: 'black', borderWidth: 1, padding: 10}}
+          placeholder="Goal" style={styles.textInputField}
+          onChangeText={textInputHandler} //no () to update on keystroke instead of instantly
+          value={enteredText}
         />
-        <Button title="ADD"/>
+        <Button title="ADD" onPress={addTextHandler}/>
       </View>
-      <View id="viewOutput">
-      </View>
+      <ScrollView id="viewOutputContainer">
+        {storedText.map(text => <View key={text} style={styles.textOutputListItem}><Text>{text}</Text></View>)}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  viewRootContainer: {
+    padding: 50
+  },
+  viewInputContainer: {
+    flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'
+  },
+  textInputField: {
+    width: '80%', borderColor: 'black', borderWidth: 1, padding: 10
+  },
+  textOutputListItem: {
+    padding: 10, margin: 10, backgroundColor: 'grey', borderColor: 'black', borderWidth: 1
+  }
 });
